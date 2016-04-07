f1c1cd70a940235ede2cc0837455dd3e623c7a69
Slsvcn <sebastian.solari.16@gmail.com>
2016-04-07 02:24:36 -0500

    Primer commit, añadí .gitignore

---
+++ src/main/java/com/clubsis/ClubSisApplication.java
@@ -0,0 +1,12 @@
+package com.clubsis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubSisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubSisApplication.class, args);
+	}
+}
