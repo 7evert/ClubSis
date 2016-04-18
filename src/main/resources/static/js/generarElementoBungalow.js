0127f9d82d3c16e903fb5b2c700e22c5f26ffb99
a20111371 <a20111371@pucp.pe>
2016-04-18 13:21:17 -0500

    Cambios en Header, para referenciar el amntenimiento de Bungalows Estilos par la lista Bungalows, para cada bungalow  que se mostrara o cualquier otra lista quizas Estilos de la plantilla de botones que se mostrara por cada usuario acciones o botones diferentes generar Elemento BUngalow, genera dinamicamente un elemento de la lista de bungalow plantilla de botones.js-> genera dinamicamente los botones por cada elemento de la lista de acuerdo al tipo de usuario mantenimiento HTML-> LLAMA A TODAS LAS ANTERIORES PARA GENERAR LA LISTA DE BUNGALOWS PARA SU AMNTENIMIENTO

---
+++ src/main/resources/static/js/generarElementoBungalow.js
@@ -0,0 +1,137 @@
+/**
+ * Created by RnzPol on 18/04/2016.
+ */
+
+/************SCRIPT PARA LLENAR LOS DATOS DINAMICAMENTE*******************/
+var imagenBungalow=["img/Bungalows/TipoBungalows/1_Lorena.png",
+    "img/Bungalows/TipoBungalows/2_Club.png",
+    "img/Bungalows/TipoBungalows/3_Class.png",
+    "img/Bungalows/TipoBungalows/4_king.png",
+    "img/Bungalows/TipoBungalows/5_KingCharles.png",
+    "img/Bungalows/TipoBungalows/1_Lorena.png",
+    "img/Bungalows/TipoBungalows/2_Club.png",
+    "img/Bungalows/TipoBungalows/3_Class.png",
+    "img/Bungalows/TipoBungalows/4_king.png",
+    "img/Bungalows/TipoBungalows/5_KingCharles.png"];
+var tituloBongalow=["Bungalow Lorena","Bungalow Club","Bungalow Class","Bungalow King","Bungalow King Charles",
+    "Bungalow Lorena","Bungalow Club","Bungalow Class","Bungalow King","Bungalow King Charles"];
+var sedesBungalows=[[],[],[],[],[],[],[],[],[],[]];
+var capacidadBungalow=[6,8,12,16,20,6,8,12,16,20];
+var precioBungalow=[80.00,100.00,120.00,140.00,160.00,80.00,100.00,120.00,140.00,160.00];
+var caracteristicasBungalows=[
+    ["1 Habitacion Personal","2 Camarotes","1 Cocina","1 Sala","1 Baño"],
+    ["1 Habitacion Personal","3 Camarotes","1 Cocina","1 Sala","2 Baños"],
+    ["2 Habitaciones Personales","4 Camarotes","1 Cocina","1 Sala","2 Baños"],
+    ["3 Habitaciones Personales","5 Camarotes","1 Cocina","2 Salas","2 Baños"],
+    ["5 Habitaciones Personales","6 Camarotes","2 Cocinas","2 Salas","2 Baños"],
+    ["1 Habitacion Personal","2 Camarotes","1 Cocina","1 Sala","1 Baño"],
+    ["1 Habitacion Personal","3 Camarotes","1 Cocina","1 Sala","2 Baños"],
+    ["2 Habitaciones Personales","4 Camarotes","1 Cocina","1 Sala","2 Baños"],
+    ["3 Habitaciones Personales","5 Camarotes","1 Cocina","2 Salas","2 Baños"],
+    ["5 Habitaciones Personales","6 Camarotes","2 Cocinas","2 Salas","2 Baños"]];
+
+function retornaElementoLista(i){
+    return $('<a></a>').attr({
+        href:'#',
+        class: 'list-group-item hidden',
+    }).append(retornaPanel(i));
+}
+
+function retornaPanel(i){
+    return $('<div></div>').attr({
+        class: 'panel panel-default',
+    }).append(retornaPanelBody1(i));
+}
+
+function retornaPanelBody1(i){
+    var panel = $('<div></div>').attr({
+        class: 'panel-body',
+    });
+    var b=new Array();
+    b[0]=retornaImagenBungalow(i);
+    b[1]=retornaPanelInfo(i);
+    b[2]=retornCaracteristicas(i);
+    for(var j=0;j<b.length;j++)
+        panel.append(b[j]);
+    return panel;
+}
+
+function retornaImagenBungalow(i){
+    return $('<div></div>').attr({
+        class: 'panel-more1 imagenBungalow',
+    }).append($('<img />').attr({
+        src:imagenBungalow[i]
+    }));
+}
+
+function retornaPanelInfo(i){
+    var panelInfo=$('<div></div>').attr({
+        class: 'panel-info',
+        style: "padding-left: 2em;"
+    });
+    var b=new Array();
+    b[0]=retornaTitulo(tituloBongalow[i]);
+    b[1]==$('<p></p>');
+    b[2]=retornaSedes(sedesBungalows[i]);
+    b[3]=$('<h3></h3>').html('Capacidad');
+    b[4]=retornaCapacidad(capacidadBungalow[i]);
+    for(var j=0;j<b.length;j++)
+        panelInfo.append(b[j]);
+    return panelInfo;
+}
+function retornaTitulo(t){
+    return $('<h1></h1>').append($("<strong></strong>").html(t));
+}
+function retornaSedes(sedes){
+    if(sedes.length==0)return $('<p></p>').html("No Sedes Disponibles");
+    else {
+        var parrafo=$('<p></p>');
+        for(var i=0;i<sedes.length;i++){
+            parrafo.append($("<span></span>").attr({class:"bg-info"}).html(sedes[i]));
+        }
+        return parrafo;
+    }
+}
+function retornaCapacidad(capacidad){
+    var cadena=(capacidad>1)?" Personas":" Persona";
+    return $('<h2></h2>').html(capacidad+cadena);
+}
+
+
+function retornCaracteristicas(i){
+    var caracteristicas=$('<div></div>').attr({
+        class: 'panel-more1',
+        style: "padding-left: 2em;"
+    });
+    var b=new Array();
+    b[0]=$("<h1></h1>").html("Precio: S/. "+precioBungalow[i]);
+    b[1]=retornaMasCaracteristicas(i);
+    for(var j=0;j<b.length;j++)
+        caracteristicas.append(b[j]);
+    return caracteristicas;
+}
+
+function retornaMasCaracteristicas(index){
+    var masCaracteristicas=$('<div></div>');
+    var b=new Array();
+    b[0]=$("<h2></h2>").html("Caracteristicas:");
+    b[1]=$("<ul></ul>");
+    for(var i=0;i<caracteristicasBungalows[index].length;i++)
+        b[1].append($("<li></li>").append($("<span></span>").html(caracteristicasBungalows[index][i])));
+    for(var i=0;i<b.length;i++)
+        masCaracteristicas.append(b[i]);
+    return masCaracteristicas;
+}
+
+function retornaBotonMostrarMas(){
+    var b = $('<a></a>').attr({
+        href:'#',
+        id:'botonMostrarMas',
+        class: 'list-group-item'
+    }).append($("<h1></h1>").html("Mostrar Mas"));
+    b.on('click', function() {
+        mostrarNext5();
+        event.preventDefault();
+    });
+    return b;
+}
---
+++ src/main/resources/static/js/plantillaBotonesAccion.js
@@ -0,0 +1,72 @@
+/**
+ * Created by RnzPol on 18/04/2016.
+ */
+/*****************++PLANTILLA DE LOS BOTONES******************************/
+var superAdministrador=0,administrador=1,socio=2;
+var accionesXElemento=[
+    ["Reservar","Anular","Modificar"],//usuario:SuperAdministrador
+    ["Reservar","Anular","Modificar"],//usuario:Administradio
+    ["Reservar","Anular"]];//usuario:Socio
+var accionAgregar=[["Agregar"],//usuario:SuperAdministrador
+    [],//usuario:Administradio
+    []];//usuario:Socio
+var actualUsuario;
+function reservar(){alert("Reservar");}
+function anular(){alert("Anular");}
+function modificar(){alert("Modificar");}
+function agregar(){alert("Agregar");}
+var funciones=[reservar,anular,modificar];
+function agregarBotonesAListas(accionesXElemento,funciones,tipoUsuario){
+    var nroAcciones=accionesXElemento[tipoUsuario].length;
+    /*var divBotones=$('<div>').attr({class: 'social-box rght-box'}).appendTo($(".panel-body")[1]);*/
+    /*------------------append el div <div class="social-box rght-box">---- -*/
+    var divBotones=$('<div>').attr({
+        class: 'social-box rght-box botonesAcciones'
+    }).appendTo(".panel-body");
+
+    /*-----------------<label class="accionLista"><label>Modificar</label></label>&nbsp; */
+    for(var i=0;i<divBotones.length;i++)
+        for (var j = 0; j < nroAcciones; j++) {
+            var b = $('<lab>').attr({
+                class: 'accionLista',
+            }).append('<label>' + accionesXElemento[tipoUsuario][j] + '</label>').appendTo(divBotones[i]);
+            b[0].onclick=funciones[j];
+        }
+}
+function agregarBotonAgregar(accionAgregar,agregar,tipoUsuario){
+    if(accionAgregar[tipoUsuario].length!=0){
+        var b=$('<span>').attr({
+            class: 'pull-right',
+            id:'accionAgregar'
+        }).append('<input type="submit" tabindex="3" value="+ Agregar" class="btn btn-primary"/>').appendTo("#topMostrar");
+        b[0].onclick=agregar();
+    }
+}
+function eliminarBotones(){
+    $("#accionAgregar").remove();
+    $(".botonesAcciones").remove();
+}
+function cambiarDeUsuario(nuevoUsuario){
+    if(nuevoUsuario==actualUsuario)return;
+    actualUsuario=nuevoUsuario;
+    eliminarBotones();
+    agregarBotonAgregar(accionAgregar,agregar,nuevoUsuario);
+    agregarBotonesAListas(accionesXElemento,funciones,nuevoUsuario);
+}
+
+function mostrarNext5(){
+    var elementos=$(".list-group > .list-group-item")
+    for(var i=0,j=0;j<5 && i<elementos.length;i++){
+        if($(elementos[i]).hasClass("hidden") && elementos[i].getAttribute("id")!="botonMostrarMas"){
+            $(elementos[i]).removeClass("hidden");j++;
+        }
+    }
+    if(elementos.hasClass("hidden")==false)
+        $("#botonMostrarMas").addClass("hidden");
+}
+function llenarListaBungalows(){
+    var nroElementos=tituloBongalow.length;
+    for(var i=0;i<nroElementos;i++)
+        retornaElementoLista(i).appendTo("#listaBungalows");
+    retornaBotonMostrarMas().appendTo("#listaBungalows");
+}
