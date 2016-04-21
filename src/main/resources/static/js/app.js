0d942d39f33c43269837b83d0a3021575aaf7ac0
JoseCh09 <jose.chunga@pucp.pe>
2016-04-21 10:15:48 -0500

    evaluacion de servicio 5 estrellas reutilizable <div id="evaluacionEstrella"></div>

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,3 +1,7 @@
 /**
  * Created by Sebastian on 09-Apr-16.
  */
+//carga el codigo para puntuacion 5 estrellas
+$(function(){
+    $("#evaluacionEstrella").load("common/evaluacionEstrella.html");
+});
