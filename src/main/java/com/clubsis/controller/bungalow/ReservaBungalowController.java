db0712ea8492d6be85c62c56652549f09410c31f
Slsvcn <sebastian.solari.16@gmail.com>
2016-05-04 17:21:07 -0500

    Puse url del controlador de reserva bungalow

--- src/main/java/com/clubsis/controller/bungalow/ReservaBungalowController.java
+++ src/main/java/com/clubsis/controller/bungalow/ReservaBungalowController.java
@@ -12,6 +12,7 @@ import java.util.List;
  */
 
 @RestController
+@RequestMapping("/api/reservasbungalow")
 public class ReservaBungalowController {
     @Autowired
     private ServicioReservas servicioReservas;
