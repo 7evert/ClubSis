20198cee7414855d9c39b3500b15651b55e82967
Slsvcn <sebastian.solari.16@gmail.com>
2016-05-05 14:34:58 -0500

    Creado controlador de TipoBungalow

---
+++ src/main/java/com/clubsis/controller/bungalow/TipoBungalowController.java
@@ -0,0 +1,49 @@
+package com.clubsis.controller.bungalow;
+
+import com.clubsis.model.bungalow.TipoBungalow;
+import com.clubsis.service.ServicioReservas;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by Sebastian on 05-May-16.
+ */
+
+@RestController
+@RequestMapping("/api/tiposbungalow")
+public class TipoBungalowController {
+    @Autowired
+    private ServicioReservas servicioReservas;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<TipoBungalow> list() {
+        // TODO: retornar todos los tipos de bungalows (llamar a un solo método del servicio que haga esto)
+        return null;
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    public TipoBungalow get(@PathVariable Integer id) {
+        // TODO: retornar un tipo de bungalow (llamar a un solo método del servicio que haga esto)
+        return null;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public TipoBungalow create(@RequestBody TipoBungalow bungalow) {
+        // TODO: crear un tipo de bungalow (llamar a un solo método del servicio que haga esto)
+        return null;
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public TipoBungalow update(@PathVariable Integer id, @RequestBody TipoBungalow bungalow) {
+        // TODO: actualizar un tipo de bungalow (llamar a un solo método del servicio que haga esto)
+        return null;
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public TipoBungalow delete(@PathVariable Integer id) {
+        // TODO: eliminar un tipo de bungalow (llamar a un solo método del servicio que haga esto)
+        return null;
+    }
+}
