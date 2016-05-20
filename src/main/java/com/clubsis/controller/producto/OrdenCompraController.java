883e3d33822f0bee927d291aaa4c1c14155a1f89
viivigood <vivian0306@hotmail.com>
2016-05-20 03:23:18 -0500

    Ningun cambio importante

--- src/main/java/com/clubsis/controller/producto/OrdenCompraController.java
+++ src/main/java/com/clubsis/controller/producto/OrdenCompraController.java
@@ -1,7 +1,13 @@
 package com.clubsis.controller.producto;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
 /**
  * Created by Vivian on 20/05/2016.
  */
+@RestController
+@RequestMapping("/api/ordenCompra")
 public class OrdenCompraController {
 }
