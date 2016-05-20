b6c16098846a2fdd2d97c30d2281822075f02514
The-Blitz <a20112387@pucp.pe>
2016-05-20 00:36:51 -0500

    Membresia , Solicitud suspension

--- src/main/java/com/clubsis/controller/persona/SuspensionController.java
+++ src/main/java/com/clubsis/controller/persona/SuspensionController.java
@@ -5,6 +5,8 @@ import com.clubsis.service.ServicioMembresias;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 /**
  * Created by Juan Tenorio on 17/5/2016.
  */
@@ -14,9 +16,23 @@ public class SuspensionController {
     @Autowired
     private ServicioMembresias servicioMembresias;
 
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Suspension> list(){return servicioMembresias.mostrarSuspensiones();}
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Suspension get(@PathVariable Integer id){
+        return servicioMembresias.buscarSuspension(id);
+    }
+
     @RequestMapping(method = RequestMethod.POST)
     public Suspension create(@RequestBody Suspension suspension){
-        return null;
+        return servicioMembresias.crearSuspension(suspension);
     }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public Suspension update(@PathVariable Integer id, @RequestBody Suspension suspension){
+        return servicioMembresias.actualizarSuspension(id,suspension);
+    }
+
 
 }
