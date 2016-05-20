551546fea3fa29e939bfeea0e7931ca777446c75
viivigood <vivian0306@hotmail.com>
2016-05-20 03:58:02 -0500

    Repositorios Producto

---
+++ src/main/java/com/clubsis/repository/producto/OrdenCompraRepository.java
@@ -0,0 +1,10 @@
+package com.clubsis.repository.producto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.clubsis.model.producto.OrdenCompra;
+
+/**
+ * Created by Vivian on 20/05/2016.
+ */
+public interface OrdenCompraRepository extends JpaRepository<OrdenCompra, Integer> {
+}
