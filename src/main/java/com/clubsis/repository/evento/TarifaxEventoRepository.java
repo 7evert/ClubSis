4ca0a9c28f653aa4e0325cf30f23cc64c1a7f921
Juanjo <xshinobi12x@yahoo.com.pe>
2016-05-22 21:20:10 -0500

    si la hacemos, con fe

---
+++ src/main/java/com/clubsis/repository/evento/TarifaxEventoRepository.java
@@ -0,0 +1,10 @@
+package com.clubsis.repository.evento;
+
+import com.clubsis.model.evento.TarifaEvento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by Juan Tenorio on 22/5/2016.
+ */
+public interface TarifaxEventoRepository extends JpaRepository<TarifaEvento, Integer> {
+}
