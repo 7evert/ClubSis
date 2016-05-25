ef582b0c7ea9d023ec828de42557595a66fcbc29
The-Blitz <a20112387@pucp.pe>
2016-05-25 03:40:40 -0500

    Administracion Pagos , BACK

---
+++ src/main/java/com/clubsis/repository/pago/CuotaExtraordinariaRepository.java
@@ -0,0 +1,10 @@
+package com.clubsis.repository.pago;
+
+import com.clubsis.model.pago.CuotaExtraordinaria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by Blitz on 25/05/2016.
+ */
+public interface CuotaExtraordinariaRepository extends JpaRepository<CuotaExtraordinaria, Integer> {
+}
