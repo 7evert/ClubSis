19f12f2e2e2bcfbf61aacd8e69b1be6b3b75c2d5
Juanjo <xshinobi12x@yahoo.com.pe>
2016-05-26 02:43:29 -0500

    clases de equivalencia + campo telefono

--- src/main/java/com/clubsis/model/sede/Sede.java
+++ src/main/java/com/clubsis/model/sede/Sede.java
@@ -23,6 +23,7 @@ public class Sede {
     private String nombre;
     private String direccion;
     private String descripcion;
+    private String telefono;
 
     @JsonIgnore
     @OneToMany(mappedBy = "sede",cascade=CascadeType.ALL)
@@ -41,12 +42,15 @@ public class Sede {
     protected Sede() {
     }
 
-    public Sede(String nombre, String direccion, String descripcion, Set<Bungalow> bungalows, Set<Evento> eventos) {
+    public Sede(String nombre, String direccion, String descripcion, String telefono, Set<Evento> eventos, Set<Bungalow> bungalows, Set<OrdenCompra> ordenes, Set<Proveedor> proveedores) {
         this.nombre = nombre;
         this.direccion = direccion;
         this.descripcion = descripcion;
-        this.bungalows = bungalows;
+        this.telefono = telefono;
         this.eventos = eventos;
+        this.bungalows = bungalows;
+        this.ordenes = ordenes;
+        this.proveedores = proveedores;
     }
 
     public Integer getId() {
@@ -81,6 +85,14 @@ public class Sede {
         this.descripcion = descripcion;
     }
 
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
     public Set<Bungalow> getBungalows() {
         return bungalows;
     }
@@ -95,5 +107,21 @@ public class Sede {
 
     public void setEventos(Set<Evento> eventos) {
         this.eventos = eventos;
+    }
+
+    public Set<Proveedor> getProveedores() {
+        return proveedores;
+    }
+
+    public void setProveedores(Set<Proveedor> proveedores) {
+        this.proveedores = proveedores;
+    }
+
+    public Set<OrdenCompra> getOrdenes() {
+        return ordenes;
+    }
+
+    public void setOrdenes(Set<OrdenCompra> ordenes) {
+        this.ordenes = ordenes;
     }
 }
