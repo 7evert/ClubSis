f0d2d3de14585ce8d24c4e335669adeb6df46efe
LuisLoo <loo.luis@pucp.pe>
2016-06-08 18:48:47 -0500

    ll: Mantenimiento Tipo de bungalo completo y añadido a la barra de menu de Club->Tipo de Bungalows

--- src/main/java/com/clubsis/controller/bungalow/TipoBungalowController.java
+++ src/main/java/com/clubsis/controller/bungalow/TipoBungalowController.java
@@ -1,8 +1,12 @@
 package com.clubsis.controller.bungalow;
 
+import com.clubsis.model.bungalow.Bungalow;
+import com.clubsis.model.bungalow.EstadoBungalow;
+import com.clubsis.model.bungalow.EstadoTipoBungalow;
 import com.clubsis.model.bungalow.TipoBungalow;
 import com.clubsis.service.ServicioReservas;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -37,12 +41,20 @@ public class TipoBungalowController {
     @RequestMapping(value = "{id}", method = RequestMethod.PUT)
     public TipoBungalow update(@PathVariable Integer id, @RequestBody TipoBungalow tipoBungalow) {
         // TODO: actualizar un tipo de bungalow (llamar a un solo método del servicio que haga esto)
-        return null;
+        return servicioReservas.actualizarTipoBungalow(id,tipoBungalow);
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
     public TipoBungalow delete(@PathVariable Integer id) {
         // TODO: eliminar un tipo de bungalow (llamar a un solo método del servicio que haga esto)
         return null;
+    }
+
+    @RequestMapping(value = "/eliminar", method = RequestMethod.POST)
+    public Integer editarPostulante(Model model, @RequestParam(value = "idTipoBungalow") Integer idTipoBungalow) {
+        TipoBungalow tipoBungalow = servicioReservas.obtenerBungalow(idTipoBungalow);
+        tipoBungalow.setEstado(EstadoTipoBungalow.INHABILITADO); //Lo deshabilitamos de la base de datos
+        servicioReservas.actualizarTipoBungalow(idTipoBungalow,tipoBungalow);
+        return 1;
     }
 }
---
+++ src/main/java/com/clubsis/model/bungalow/EstadoTipoBungalow.java
@@ -0,0 +1,8 @@
+package com.clubsis.model.bungalow;
+
+/**
+ * Created by Luis on 08/06/2016.
+ */
+public enum EstadoTipoBungalow {
+    CREADO, DISPONIBLE, INHABILITADO, ELIMINADO
+}
