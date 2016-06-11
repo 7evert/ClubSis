aecd94d2946e14cc3ab383f5e68565a2e05da325
viivigood <vivian0306@hotmail.com>
2016-06-11 04:28:47 -0500

    SEDES Y PRODUCTOS

--- src/main/java/com/clubsis/service/ServicioSedes.java
+++ src/main/java/com/clubsis/service/ServicioSedes.java
@@ -1,6 +1,11 @@
 package com.clubsis.service;
 
+import com.clubsis.model.producto.Producto;
+import com.clubsis.model.producto.Producto_Sede;
+import com.clubsis.model.producto.Producto_SedeID;
 import com.clubsis.model.sede.Sede;
+import com.clubsis.repository.producto.ProductoRepository;
+import com.clubsis.repository.producto.Producto_SedeRepository;
 import com.clubsis.repository.sede.SedeRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +21,24 @@ public class ServicioSedes {
     // Sede
     @Autowired
     private SedeRepository sedeRepository;
+    @Autowired
+    private ProductoRepository productoRepository;
+    @Autowired
+    private Producto_SedeRepository producto_sedeRepository;
+
     public List<Sede> mostrarSedes() {
         return sedeRepository.findAll();
     }
 
     public Sede crearSede(Sede sede) {
-        return sedeRepository.saveAndFlush(sede);
+        Sede nuevaSede=sedeRepository.saveAndFlush(sede);
+        List<Producto> productos = productoRepository.findAll();
+        for (Producto item : productos) {
+            Producto_SedeID id = new Producto_SedeID(productoRepository.findOne(item.getId()),nuevaSede);
+            Producto_Sede stock = new Producto_Sede(id,0);
+            producto_sedeRepository.saveAndFlush(stock);
+        }
+        return nuevaSede;
     }
     public Sede buscarSede(Integer id){  return sedeRepository.findOne(id);}
 
