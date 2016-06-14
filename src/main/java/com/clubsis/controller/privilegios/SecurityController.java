260a5312a882ccb6f0618398eda69230767fcde3
goldfenrir <goldfenrir@hotmail.com>
2016-06-13 19:13:17 -0500

    Security controller

---
+++ src/main/java/com/clubsis/controller/privilegios/SecurityController.java
@@ -0,0 +1,21 @@
+package com.clubsis.controller.privilegios;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by goldfenrir on 6/13/2016.
+ */
+@RestController
+@RequestMapping("/api/security")
+public class SecurityController {
+    @RequestMapping(method = RequestMethod.POST)
+    public List<Integer> obtenerRol(@RequestBody String cadena){
+        return null;
+    }
+    
+}
