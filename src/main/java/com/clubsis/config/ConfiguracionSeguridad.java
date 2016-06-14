30ac73a840916cda24ed37442e553b6d4af97e2a
Slsvcn <sebastian.solari.16@gmail.com>
2016-06-14 01:17:54 -0500

    Login simple funciona

--- src/main/java/com/clubsis/config/ConfiguracionSeguridad.java
+++ src/main/java/com/clubsis/config/ConfiguracionSeguridad.java
@@ -1,22 +1,11 @@
 package com.clubsis.config;
 
-import org.springframework.boot.autoconfigure.security.Http401AuthenticationEntryPoint;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Created by Sebastian on 13-Jun-16.
@@ -33,35 +22,15 @@ public class ConfiguracionSeguridad extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
             .authorizeRequests()
-                /*.antMatchers("/common/**").permitAll()
-                .antMatchers("/img/**").permitAll()
-                .antMatchers("/webjars/**").permitAll()
-                .antMatchers("/", "/index.html").permitAll()*/
                 .anyRequest().authenticated()
-                .and().httpBasic().and()
-            .exceptionHandling()
-                // poner un authenticationEntryPoint causa que ya no se redirija automáticamente a la página del login
-                .authenticationEntryPoint(new AuthenticationEntryPoint() {
-                    @Override
-                    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
-                        httpServletResponse.sendError(401, e.getMessage());
-                    }
-                })
                 .and()
             .formLogin()
-                //.defaultSuccessUrl("/")
+                .loginPage("/login")
                 .permitAll()
-                .successHandler(new SimpleUrlAuthenticationSuccessHandler() {
-                    @Override
-                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        clearAuthenticationAttributes(request);
-                    }
-                })
-                .failureHandler(new SimpleUrlAuthenticationFailureHandler())
                 .and()
-            .logout();
-        //http.exceptionHandling().authenticationEntryPoint(new Http401AuthenticationEntryPoint())
+            .logout()
+                .permitAll();
     }
 }
